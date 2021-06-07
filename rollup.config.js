b2afb2653d4abad8d338e28c1f18151477bfd7d7
Matsuuu <huhta.matias@gmail.com>
2021-06-07 12:58:42 +0300

    Messaging protocol rework, Maybe this time it's better?

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,30 @@
-import resolve from '@rollup/plugin-node-resolve';
-import html from '@open-wc/rollup-plugin-html';
+import resolve from "@rollup/plugin-node-resolve";
+import html from "@open-wc/rollup-plugin-html";
 
-export default [{
-    input: 'html/lit-devtools-chrome.html',
-    output: { dir: 'dist' },
-    plugins: [
-        html({
-            minify: false,
-        }),
-        resolve(),
-    ],
-}, {
-    input: './lib/content/content_script.js',
-    output: { dir: 'dist' },
-    plugins: [
-        resolve()
-    ]
-}, {
-    input: './lib/background/background.js',
-    output: { dir: 'dist' },
-    plugins: [
-        resolve()
-    ]
-}
+export default [
+    {
+        input: "html/lit-devtools-chrome.html",
+        output: { dir: "dist" },
+        plugins: [
+            html({
+                minify: false,
+            }),
+            resolve(),
+        ],
+    },
+    {
+        input: "html/lit-devtools-chrome-init.html",
+        output: { dir: "dist" },
+        plugins: [resolve(), html({ minify: false })],
+    },
+    {
+        input: "./lib/content/content_script.js",
+        output: { dir: "dist" },
+        plugins: [resolve()],
+    },
+    {
+        input: "./lib/background/background.js",
+        output: { dir: "dist" },
+        plugins: [resolve()],
+    },
 ];
