83992184a20f29813cdd895d81054d854c27a6b8
Matsuuu <huhta.matias@gmail.com>
2021-09-16 20:36:27 +0300

    Decrease packaging size and optimize injection
    - Remove the old injection method
    - Replace old injection method with a module import approach

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
-import resolve from "@rollup/plugin-node-resolve";
-import html from "@open-wc/rollup-plugin-html";
-import copy from "rollup-plugin-copy";
+import resolve from '@rollup/plugin-node-resolve';
+import html from '@open-wc/rollup-plugin-html';
+import copy from 'rollup-plugin-copy';
 
 export default [
     {
-        input: "html/wc-devtools.html",
-        output: { dir: "dist" },
+        input: 'html/wc-devtools.html',
+        output: { dir: 'dist' },
         plugins: [
             html({
                 minify: false,
@@ -13,51 +13,38 @@ export default [
             copy({
                 targets: [
                     { src: 'icons/*', dest: 'dist' },
-                    { src: "manifest.json", dest: 'dist' }
-                ]
+                    { src: 'manifest.json', dest: 'dist' },
+                ],
             }),
             resolve(),
         ],
     },
     {
-        input: "html/wc-devtools-init.html",
-        output: { dir: "dist" },
+        input: 'html/wc-devtools-init.html',
+        output: { dir: 'dist' },
         plugins: [resolve(), html({ minify: false })],
     },
     {
-        input: "html/wc-devtools-background.html",
-        output: { dir: "dist" },
+        input: 'html/wc-devtools-background.html',
+        output: { dir: 'dist' },
         plugins: [resolve(), html({ minify: false })],
     },
     {
         input: {
-            content_script: "./lib/content/content_script.js",
-            nydus: "./packages/nydus/nydus.js",
-            analyzer: "./packages/analyzer/index.js",
-            background: "./lib/background/background.js",
-            crawler_constants: "./lib/crawler/crawler-constants.js",
-            crawler_inject: "./lib/crawler/crawler-inject.js",
-            spotlight_border: "./lib/elements/spotlight-border.js",
-            content_messaging: "./lib/content/content-messaging.js"
+            content_script: './lib/content/content_script.js',
+            nydus: './packages/nydus/nydus.js',
+            analyzer: './packages/analyzer/index.js',
+            background: './lib/background/background.js',
+            "crawler-constants": './lib/crawler/crawler-constants.js',
+            'crawler-inject': './lib/crawler/crawler-inject.js',
+            'spotlight-border': './lib/elements/spotlight-border.js',
+            'content-messaging': './lib/content/content-messaging.js',
         },
-        output: { dir: "dist" },
-        plugins: [resolve({
-            dedupe: ["nydus", "analyzer"],
-
-        })],
+        output: { dir: 'dist' },
+        plugins: [
+            resolve({
+                dedupe: ['nydus', 'analyzer'],
+            }),
+        ],
     },
 ];
-
-/*
-    {
-        input: {
-            background: "./lib/background/background.js",
-            nydus: "./packages/nydus/nydus.js",
-            analyzer: "./packages/analyzer/index.js"
-        },
-        output: { dir: "dist" },
-        plugins: [resolve({
-            dedupe: ["nydus", "analyzer"]
-        })],
-    },
-    */
