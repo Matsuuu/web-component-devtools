21a9f14dddfda7d702702a128bb0fa9658302a46
Matsuuu <huhta.matias@gmail.com>
2021-09-27 23:28:14 +0300

    PoC console functionality

--- packages/playground/lib/console.js
+++ packages/playground/lib/console.js
@@ -1,7 +1,7 @@
 import { css, html, LitElement } from 'lit';
 import 'playground-elements/playground-code-editor.js';
 import gruvboxTheme from 'playground-elements/themes/gruvbox-dark.css.js';
-import { isConsoleSubmit } from './util';
+import { isConsoleClear, isConsoleSubmit } from './util';
 
 export class DevToolsConsole extends LitElement {
     static get properties() {
@@ -28,9 +28,29 @@ export class DevToolsConsole extends LitElement {
         });
     }
 
+    updated() {
+        // Hack until the value patch lands on playgrounds
+        window.requestAnimationFrame(() => {
+            window.requestAnimationFrame(() => {
+                const histories = this.commandHistory;
+                histories.forEach((hist, i) => {
+                    const historyCodes = this.shadowRoot.querySelectorAll('.history-code-editor');
+                    const historyResults = this.shadowRoot.querySelectorAll('.history-result-editor');
+                    historyCodes[i].value = hist.code;
+                    historyResults[i].value = hist.returnValue;
+                });
+            });
+        });
+    }
+
     focusEditor() {
         // Quite hacky, maybe there's a better way?
-        this.editor.shadowRoot.querySelector('.CodeMirror-code').focus();
+        /** @type HTMLElement */ (this.editor.shadowRoot.querySelector('.CodeMirror-code')).focus();
+    }
+
+    addHistoryEntry(historyEntry) {
+        this.commandHistory.push(historyEntry);
+        this.requestUpdate();
     }
 
     /**
@@ -39,25 +59,65 @@ export class DevToolsConsole extends LitElement {
     onKeyDown(e) {
         if (isConsoleSubmit(e)) {
             const consoleContent = this.editor.value;
+            if (consoleContent.trim().length <= 0) return;
+
             const submitEvent = new CustomEvent('devtools-console-submit', { detail: { code: consoleContent } });
             const eventSuccess = this.dispatchEvent(submitEvent);
             if (eventSuccess) {
                 this.editor.value = '';
             }
         }
+        if (isConsoleClear(e)) {
+            this.commandHistory = [];
+            this.requestUpdate();
+        }
     }
 
     render() {
         return html`
-            ${caret}
-            <playground-code-editor
-                class="${this.theme === 'light' ? '' : 'playground-theme-gruvbox-dark'}"
-                type="js"
-                value="console.log('foo');"
-                @keydown=${this.onKeyDown}
-            >
-            </playground-code-editor>
+            ${this.renderHistoryEntries()}
+            <span>
+                ${caret}
+                <playground-code-editor
+                    class="${this.theme === 'light' ? '' : 'playground-theme-gruvbox-dark'}"
+                    type="js"
+                    @keydown=${this.onKeyDown}
+                >
+                </playground-code-editor>
+            </span>
+            ${this.commandHistory.length <= 0 ? html`<p class="subtitle">Press Ctrl + Enter to submit</p>` : ''}
         `;
+    }
+
+    renderHistoryEntries() {
+        return this.commandHistory.map(historyEntry => {
+            let resultType = 'js';
+            if (historyEntry.error && historyEntry.errorID === '_ERR_IS_NODE') {
+                resultType = 'html';
+            }
+            return html`
+                <span>
+                    ${caret}
+                    <playground-code-editor
+                        class="history-code-editor ${this.theme === 'light' ? '' : 'playground-theme-gruvbox-dark'}"
+                        type="js"
+                        value="${historyEntry.code}"
+                        readonly
+                    >
+                    </playground-code-editor>
+                </span>
+                <span>
+                    ${resultCaret}
+                    <playground-code-editor
+                        class="history-result-editor ${this.theme === 'light' ? '' : 'playground-theme-gruvbox-dark'}"
+                        type="${resultType}"
+                        value="${historyEntry.returnValue}"
+                        readonly
+                    >
+                    </playground-code-editor>
+                </span>
+            `;
+        });
     }
 
     static get styles() {
@@ -66,10 +126,31 @@ export class DevToolsConsole extends LitElement {
             css`
                 :host {
                     display: flex;
+                    flex-direction: column;
                     --font-size: 12px;
                     font-size: var(--font-size);
                     position: relative;
                     width: 100%;
+                }
+
+                span {
+                    display: flex;
+                    width: 100%;
+                }
+
+                .command-history-entry {
+                    display: flex;
+                    flex-direction: column;
+                }
+
+                .history-command,
+                .history-result {
+                    margin: 0.25rem 2rem;
+                }
+
+                .subtitle {
+                    margin: 0 0 0 2rem;
+                    opacity: 0.6;
                 }
 
                 svg {
@@ -80,6 +161,11 @@ export class DevToolsConsole extends LitElement {
                     fill: #367cf1;
                     transform: rotate(-90deg);
                     transform-origin: center;
+                }
+
+                .result-caret {
+                    fill: var(--highlight);
+                    transform: rotate(90deg);
                 }
 
                 playground-code-editor {
@@ -97,6 +183,11 @@ if (!customElements.get('devtools-console')) {
 }
 
 const caret = html`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 451.847 451.847">
+    <path
+        d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,151.157c-12.359-12.359-12.359-32.397,0-44.751   c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0   c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z"
+    />
+</svg>`;
+const resultCaret = html`<svg class="result-caret" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 451.847 451.847">
     <path
         d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,151.157c-12.359-12.359-12.359-32.397,0-44.751   c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0   c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z"
     />
--- packages/playground/lib/util.js
+++ packages/playground/lib/util.js
@@ -2,5 +2,9 @@
  * @param {KeyboardEvent} event
  */
 export function isConsoleSubmit(event) {
-    return event.code === "Enter" && event.ctrlKey;
+    return event.key === "Enter" && event.ctrlKey;
+}
+
+export function isConsoleClear(event) {
+    return event.key === "l" && event.ctrlKey;
 }
