81b0368f889a40af3a83a0a9e57b5af2744509a7
Matsuuu <huhta.matias@gmail.com>
2024-04-04 21:09:44 +0300

    Update some of the example cases for more demoability

--- examples/lit/src/list-item.js
+++ examples/lit/src/list-item.js
@@ -1,27 +1,29 @@
 import { html, css, LitElement } from 'lit-element';
 
 export class ListItem extends LitElement {
-    static get properties() {
-        return {
-            selected: { type: Boolean, reflect: true },
-        };
-    }
+  static get properties() {
+    return {
+      selected: { type: Boolean, reflect: true },
+      numb: { type: Number, reflect: true }
+    };
+  }
 
-    constructor() {
-        super();
-        this.selected = false;
-    }
+  constructor() {
+    super();
+    this.selected = false;
+    this.numb = Math.floor(Math.random() * 100 + 1);
+  }
 
-    launchAlert(message) {
-        alert(message);
-    }
+  launchAlert(message) {
+    alert(message);
+  }
 
-    render() {
-        return html` <p><slot></slot></p> `;
-    }
+  render() {
+    return html` <p><slot></slot> ${this.numb}</p> `;
+  }
 
-    static get styles() {
-        return css`
+  static get styles() {
+    return css`
             :host {
                 display: block;
             }
@@ -30,7 +32,7 @@ export class ListItem extends LitElement {
                 background: rgba(0, 0, 200, 0.4);
             }
         `;
-    }
+  }
 }
 
 customElements.define('list-item', ListItem);
