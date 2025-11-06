10e44e1804f8b832078ae88aa5fdef79c6aec770
Matsuuu <huhta.matias@gmail.com>
2025-11-06 21:28:41 +0200

    Add Spotlight element todos

--- src/pages/content/lib/spotlight/spotlight-element.ts
+++ src/pages/content/lib/spotlight/spotlight-element.ts
@@ -2,6 +2,13 @@ const ELEM_ID = "wc-devtools-spotlight-element";
 let currentElement: HTMLDivElement | undefined = undefined;
 let spotlightRemoveTimeout: NodeJS.Timeout;
 
+// TODO: Currently the spotlight can be a bit finnicky. We need to more reliably get rid of it
+// as we go further so it won't be a burdain.
+
+// TODO: Spotlight should have a minimum size of 1px x 1px for those 0x0 elements.
+
+// TODO: Spotlight should have the dimensions of the object and it's name.
+
 export function getSpotlightElement() {
     if (currentElement) {
         setTimeout(() => {
